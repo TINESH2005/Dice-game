8864e5915a2d3f9e0e867c67c5cc17a9399b54c1
TINESH2005 <kalitinesh65@gmail.com>
2024-09-21 18:39:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.img1').setAttribute('src', `/images/dice${randomNumber1}.png`);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.img2').setAttribute('src', `/images/dice${randomNumber2}.png`);
+var header = document.querySelector('h1');
+if (randomNumber1 > randomNumber2) {
+  header.innerHTML = "Player 1 Wins! 🎉";
+} else if (randomNumber1 < randomNumber2) {
+  header.innerHTML = "Player 2 Wins! 🎉";
+} else {
+  header.innerHTML = "It's a Draw! 🤝";
+}
